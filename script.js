8eb60108c7112d75a34036c32b98d973c4588951
Diskotjej <kamillafagerland1@gmail.com>
2025-01-15 01:42:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,33 +3,12 @@
 //------------------------------------------------
 
 const folderPath = './image%20folder/';
-
 let images = [
-  "IMG_0823.jpg", "IMG_0832.jpg", "IMG_1092.jpg", "IMG_1160.jpg", "IMG_1164.jpg",
-  "IMG_1167.jpg", "IMG_1168.jpg", "IMG_1171.jpg", "IMG_1174(1).jpg", "IMG_1174.jpg",
-  "IMG_1177.jpg", "IMG_1178.jpg", "IMG_1185.jpg", "IMG_1189.jpg", "IMG_1191.jpg",
-  "IMG_1193.jpg", "IMG_1194.jpg", "IMG_1197.jpg", "IMG_1198.jpg", "IMG_1199.jpg",
-  "IMG_1202.jpg", "IMG_1204.jpg", "IMG_1208.jpg", "IMG_1213.jpg", "IMG_1215.jpg",
-  "IMG_1217.jpg", "IMG_1219.jpg", "IMG_1368.jpg", "IMG_1384.png", "IMG_1440.jpg",
-  "IMG_1637.jpg", "IMG_1648.jpg", "IMG_1679.jpg", "IMG_1681.jpg", "IMG_1682.jpg",
-  "IMG_1685.jpg", "IMG_2460.jpg", "IMG_2524.jpg", "IMG_2530_25-04-2019-19-43-53.jpg",
-  "IMG_2543.jpg", "IMG_2547.jpg", "IMG_2562.jpg", "IMG_2568.jpg", "IMG_2578__29-04-2019-02-35-30.jpg",
-  "IMG_2658.jpg", "IMG_2676.jpg", "IMG_2980.png", "IMG_4020.jpg", "IMG_4379.jpg",
-  "IMG_4410.jpg", "IMG_4420.jpg", "IMG_4440.jpg", "IMG_4465.jpg", "IMG_4468.jpg",
-  "IMG_4474.jpg", "IMG_4496.jpg", "IMG_4597.jpg", "IMG_4619.jpg", "IMG_5365.jpeg",
-  "IMG_5668.jpg", "IMG_5999.jpg", "IMG_6001.jpg", "IMG_6009.jpg", "IMG_6010.jpg",
-  "IMG_6174.jpeg", "IMG_6304.jpg", "IMG_6337.jpg", "IMG_6339.jpg", "IMG_6419.jpg",
-  "IMG_6476.jpg", "IMG_6503.jpg", "IMG_6979.jpg", "IMG_7060.jpg", "IMG_7247.jpg",
-  "IMG_7479.png", "IMG_7502.jpeg", "IMG_7510.jpg", "IMG_8057.jpg", "IMG_8062.jpg",
-  "IMG_8098.jpeg", "IMG_8116.jpg", "IMG_8325.jpg", "IMG_8765.jpeg", "IMG_8973.jpg",
-  "IMG_9082.jpg", "images_photo15.jpg", "images_photo23.jpg", "images_photo24.jpeg",
-  "images_photo26.jpg", "images_photo27.jpeg", "images_photo28.jpeg", "images_photo3.jpg",
-  "images_photo30.jpg", "images_photo32.jpg", "images_photo34.jpg", "images_photo35.jpg",
-  "images_photo4(1).jpg", "images_photo4.jpg", "images_photo6.jpg", "images_photo7.jpg"
+  "IMG_0823.jpg", "IMG_0832.jpg", "IMG_1092.jpg", /* ... etc ... */ "images_photo7.jpg"
 ];
 
-// Clone of the original array for reset purposes
-let imagesBackup = [...images];
+// // REMOVED: We do NOT need the backup anymore
+// let imagesBackup = [...images];
 
 //------------------------------------------------
 // 2) SIZES & PATTERNS
@@ -68,8 +47,11 @@ const verticalGap = 4;
 //------------------------------------------------
 // 4) INITIALIZE
 //------------------------------------------------
+
+let spawnTimer; // NEW: Keep a reference to the setInterval
+
 function initializeAnimation() {
-  setInterval(() => {
+  spawnTimer = setInterval(() => {
     createRandomRow();
   }, spawnInterval);
 
@@ -81,9 +63,17 @@ function initializeAnimation() {
 // 5) CREATE & ANIMATE A ROW
 //------------------------------------------------
 function createRandomRow() {
+  // NEW: If all images are used up, stop
   if (images.length === 0) {
-    images = [...imagesBackup]; // Refill after all images are shown once
+    // Optional: clear the interval so it doesn't run forever
+    clearInterval(spawnTimer);
+    return;
   }
+
+  // REMOVED: no more refill
+  // if (images.length === 0) {
+  //   images = [...imagesBackup];
+  // }
 
   const pattern = patterns[Math.floor(Math.random() * patterns.length)];
   const container = document.getElementById('floating-container');
@@ -91,7 +81,10 @@ function createRandomRow() {
   let maxHeight = 0;
 
   pattern.sizeKeys.forEach((sizeKey, i) => {
-    const imageName = images.shift(); // Always use the first image in the array
+    // Again, guard: if we are literally about to run out mid-pattern
+    if (images.length === 0) return;
+
+    const imageName = images.shift(); // remove from the front => never repeats
 
     const { width, height } = sizes[sizeKey];
     if (height > maxHeight) {
